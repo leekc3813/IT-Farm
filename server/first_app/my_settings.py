618f6788807de4a9e9d14e2c54c29da7a9f9a767
1715leekc <1715leekc@naver.com>
2023-10-25 11:04:25 +0900

    Feat: setting
    client, server 초기세팅

---
+++ server/first_app/my_settings.py
@@ -0,0 +1,13 @@
+#mysettings.py
+DATABASES = DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'it_farm',
+        'USER': 'root',
+        'PASSWORD': 'mysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = "django-insecure-z112d1h4)cot408pe433pv8x!a#frygjsxslh)ik21a(6qmok@"
