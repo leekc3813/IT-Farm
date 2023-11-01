85576c1347abbc9b3e0ef804860b2a7b218bcf7d
1715leekc <1715leekc@naver.com>
2023-11-01 10:39:14 +0900

    Styles: home
    홈 로고변경

--- client/src/component/home/home.presenter.tsx
+++ client/src/component/home/home.presenter.tsx
@@ -7,15 +7,13 @@ export function HomePageUI (props:IHomePageUIProps):JSX.Element {
         <div className={styles.homeBody}>
             <div className={styles.homeWrapper}>
                 <div className={styles.titleContainer}> 
-                    <img src='/img/home/logo_2green2.png' className={styles.logo} />
+                    <img src='/img/home/homelogo.png' className={styles.logo} />
                 </div>
                 <div className={styles.buttonContainer}>
-                    <button className={styles.selectButton}>사러왔어요.</button>
+                    <button onClick = {props.onCLickCustom} className={styles.selectButton}>사러왔어요.</button>
                     <button onClick={props.onClickAdmin} className={styles.selectButton}>팔러왔어요</button>
                 </div>
-                
-            </div>
-            
+            </div>      
         </div>
     )
 }
