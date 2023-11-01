86b259977b4e31023ffc04a154bd518a2814c350
aram lee <mia.aram.lee@gmail.com>
2023-11-01 10:03:22 +0900

    Styles: Home
    홈디자인

--- client/src/component/home/home.presenter.tsx
+++ client/src/component/home/home.presenter.tsx
@@ -1,21 +1,21 @@
 import styles from './home.module.css';
 import { IHomePageUIProps } from './home.types';
+import Image from '/img/home/background.png';
 
 export function HomePageUI (props:IHomePageUIProps):JSX.Element {
     return (
         <div className={styles.homeBody}>
             <div className={styles.homeWrapper}>
-                <div className={styles.titleContainer}>
-                    <img src='/img/home/homelogos.png' className={styles.logo} />
-                    <div className={styles.logoTitle}>
-                        IT-Farm
-                    </div>
+                <div className={styles.titleContainer}> 
+                    <img src='/img/home/logo_2green2.png' className={styles.logo} />
                 </div>
                 <div className={styles.buttonContainer}>
-                    <button onClick={props.onCLickCustom} className={styles.selectButton}>사러왔어요</button>
-                    <button onClick={props.onClickAdmin} className={styles.selectButton}>판매할래요</button>
+                    <button className={styles.selectButton}>사러왔어요.</button>
+                    <button onClick={props.onClickAdmin} className={styles.selectButton}>팔러왔어요</button>
                 </div>
+                
             </div>
+            
         </div>
     )
 }
