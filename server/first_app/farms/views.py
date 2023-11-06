6cdf6c0e802caacd69d7d3a45593340bc3d6a067
ehgmlrjs <ehgmlrjs@naver.com>
2023-11-06 14:30:39 +0900

    Feat: lat,log
    주소를 위도,경도로 변환
    가장 가까운 센터 등록

--- server/first_app/farms/views.py
+++ server/first_app/farms/views.py
@@ -6,7 +6,7 @@ from .serializers import *
 from .models import Farms
 from django.shortcuts import get_object_or_404
 from users.views import AuthView
-from .distance import distance
+from .distance import geocoding, distance
 
 
 class FarmCreateView(APIView):
@@ -19,8 +19,9 @@ class FarmCreateView(APIView):
         farms = Farms.objects.filter(user_id=user_id,address=address,address_detail=address_detail)
         if farms.exists():
             return Response({"message":"이미 등록된 농장입니다."}, status=status.HTTP_409_CONFLICT)
-        center = distance(address)
-        serializer = FarmSerializer(data={**request.data, 'user_id':user_id, 'center':center})
+        latitude, longitude = geocoding(address)
+        center = distance(latitude,longitude)
+        serializer = FarmSerializer(data={**request.data, 'user_id':user_id, 'center':center, 'latitude':latitude, 'longitude':longitude})
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "성공"}, status=status.HTTP_201_CREATED)
@@ -53,6 +54,7 @@ class FarmDeleteView(APIView):
             return Response({'message':'삭제'}, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'message':f'삭제 실패: {e}'}, status=status.HTTP_400_REQUEST)
+        
 class FarmReadView(APIView):
     def post(self, request):
         auth_view = AuthView()
