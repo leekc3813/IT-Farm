5e2667fb1971fb74c575a2d5184e1955cd9b386c
aram lee <mia.aram.lee@gmail.com>
2023-11-07 15:07:30 +0900

    design: 디자인 추가
    공지사항 게시판, 글쓰기 페이지 추가

---
+++ client/pages/purchase/board/notice/index.tsx
@@ -0,0 +1,7 @@
+import NoticePage from "@/src/component/purchase/board/notice/notice.container";
+
+export default function Notice():JSX.Element {
+    return(
+        <NoticePage />
+    )
+}
---
+++ client/src/component/purchase/board/qna/qna.container.tsx
@@ -0,0 +1,7 @@
+import QnaPageUI from "./qna.presenter";
+
+export default function QnaPage():JSX.Element{
+    return(
+        <QnaPageUI />
+    )
+}
