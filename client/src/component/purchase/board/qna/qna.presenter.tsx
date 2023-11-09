b15cb3084b0eff7278673a047ceb5279714fa8c6
1715leekc <1715leekc@naver.com>
2023-11-09 14:37:05 +0900

    Fix: qna
    qna 수정

--- client/src/component/purchase/board/qna/qna.presenter.tsx
+++ client/src/component/purchase/board/qna/qna.presenter.tsx
@@ -4,9 +4,9 @@ import { IQnaPageUIProps } from "./qna.types";
 export default function QnaPageUI(props: IQnaPageUIProps):JSX.Element{
     return(
         <div className={styles.body}>
-          {props.data[0].subject}
-          {props.data[0].regdate}
-          {props.data[0].content}
+          {props.data[0]?.subject}
+          {props.data[0]?.regdate}
+          {props.data[0]?.content}
         </div>
     )
 }
