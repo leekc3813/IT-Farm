e93d2e58744cecf8fee38ef40c036ea7d2b991ee
1715leekc <1715leekc@naver.com>
2023-11-10 09:49:25 +0900

    Feat: purchase/list
    상품목록 디자인

--- client/src/component/purchase/list/purchaseList.presenter.tsx
+++ client/src/component/purchase/list/purchaseList.presenter.tsx
@@ -8,7 +8,111 @@ export default function PurchaseListPageUI():JSX.Element {
                     홍고추
                 </div>
                 <div className={styles.contentContainer}>
-                    
+                    <div className={styles.contentBox}>
+                        <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>유기농 홍고추</p>
+                            <p className={styles.contentsPrice}>12000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>무농약 홍고추</p>
+                            <p className={styles.contentsPrice}>10000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>일반 홍고추</p>
+                            <p className={styles.contentsPrice}>8000원 1kg</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    청고추
+                </div>
+                <div className={styles.contentContainer}>
+                    <div className={styles.contentBox}>
+                        <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>유기농 홍고추</p>
+                            <p className={styles.contentsPrice}>12000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>무농약 홍고추</p>
+                            <p className={styles.contentsPrice}>10000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>일반 홍고추</p>
+                            <p className={styles.contentsPrice}>8000원 1kg</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    건고추
+                </div>
+                <div className={styles.contentContainer}>
+                    <div className={styles.contentBox}>
+                        <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>유기농 홍고추</p>
+                            <p className={styles.contentsPrice}>12000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>무농약 홍고추</p>
+                            <p className={styles.contentsPrice}>10000원 1kg</p>
+                        </div>
+                    </div>
+                    <div className={styles.contentBox}>
+                         <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>일반 홍고추</p>
+                            <p className={styles.contentsPrice}>8000원 1kg</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    고추가루
+                </div>
+                <div className={styles.contentContainer}>
+                    <div className={styles.contentBox}>
+                        <div className={styles.imgBox}>
+                            <img src='https://via.placeholder.com/321x221/FFFF00/000000' alt='상품이미지' />
+                        </div>
+                        <div className={styles.contentsBox}>
+                            <p className={styles.contentsTitle}>국산 고추가루</p>
+                            <p className={styles.contentsPrice}>12000원 1kg</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
