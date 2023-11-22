e1c0dc69be9b809cb36487cf29a5b9cf8faf5082
1715leekc <1715leekc@naver.com>
2023-11-22 14:11:56 +0900

    Feat: center/data
    data 차트 수정

--- client/src/component/center/data/data.presenter.tsx
+++ client/src/component/center/data/data.presenter.tsx
@@ -10,19 +10,27 @@ export default function DataPageUI(props: IDataPageUIProps):JSX.Element{
         if (canvasEl.current !== null) {
           const ctx = canvasEl.current;
   
-          const labels = ['현재 생산량', '예측 생산량'];
+          const labels = ['1','2','3','4','5'];
   
           const data = {
             labels: labels,
             datasets: [
               {
-                label: 'Line Chart',
-                data: [70, 30],
+                label: '실제 생산량',
+                data: [10,20,30,40],
                 fill: false,
-                borderColor: 'rgb(75, 192, 192)',
+                borderColor : 'rgb(227, 106, 131)',
+                tension: 0.1,
+                backgroundColor: 'rgb(227, 106, 131)',
+                
+              },
+              {
+                label: '예측 생산량',
+                data: [50,10,30,20,10],
+                fill: false,
+                borderColor: 'rgb(111, 162, 247)',
                 tension: 0.1,
                 backgroundColor: [
-                    'rgb(227, 106, 131)',
                     'rgb(111, 162, 247)',
                   ],
               },
@@ -30,8 +38,15 @@ export default function DataPageUI(props: IDataPageUIProps):JSX.Element{
           };
   
           const myLineChart = new Chart(ctx, {
-            type: 'bar',
+            type: 'line',
             data: data,
+            options: {
+                scales: {
+                  y: {
+                    beginAtZero: true, // Set y-axis starting point to 0
+                  },
+                },
+              },
           });
   
           return function cleanup() {
@@ -44,39 +59,8 @@ export default function DataPageUI(props: IDataPageUIProps):JSX.Element{
         <div className={styles.body}>
             <div className={styles.wrapper}>
                 <p className={styles.title}>
-                    데이터입력
-                </p>
-                <div className={styles.inputContainer}>
-                    <div className={styles.inputBox}>
-                        <p className={styles.subtitle}>
-                            평균습도
-                        </p>
-                        <input onChange={(event) => props.onChangeDetailData('humidity',event.target.value)} className={styles.input} type='text' />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <p className={styles.subtitle}>
-                            평균기온
-                        </p>
-                        <input onChange={(event) => props.onChangeDetailData('temp',event.target.value)} className={styles.input} type='text' />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <p className={styles.subtitle}>
-                            평균 지중온도
-                        </p>
-                        <input onChange={(event) => props.onChangeDetailData('underground',event.target.value)} className={styles.input} type='text' />
-                    </div>
-                    <div className={styles.inputBox}>
-                        <p className={styles.subtitle}>
-                            복사량
-                        </p>
-                        <input onChange={(event) => props.onChangeDetailData('radiation',event.target.value)} className={styles.input} type='text' />
-                    </div>
-                </div>
-                <button onClick={props.onClickSubmit} className={styles.submitButton} >생산량 예측하기</button>
-                <p className={styles.title}>
                     예측 생산량
                 </p>
-                <input className={styles.input} type='text' readOnly value={props.result}/>
                 <div className={styles.chartBox}>
                     <canvas ref={canvasEl} />
                 </div>
